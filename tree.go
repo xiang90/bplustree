ecd9f8ed0812fb10bf398eb8472f0f0633689545
Xiang Li <xiang.li@coreos.com>
2014-04-21 12:06:45 -0400

    remove extra file

--- tree.go
+++ tree.go
@@ -87,3 +87,25 @@ func (bt *BTree) Search(key int) (string, bool) {
 	}
 	return kv.value, true
 }
+
+func search(n node, key int) (*kv, int, *leafNode) {
+	curr := n
+	oldIndex := -1
+
+	for {
+		switch t := curr.(type) {
+		case *leafNode:
+			i, ok := t.find(key)
+			if !ok {
+				return nil, oldIndex, t
+			}
+			return &t.kvs[i], oldIndex, t
+		case *interiorNode:
+			i, _ := t.find(key)
+			curr = t.kcs[i].child
+			oldIndex = i
+		default:
+			panic("")
+		}
+	}
+}
